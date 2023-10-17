713e044d8f8c6cf5705823155dc63b0d672fd799
jessicacliong <14395@coderacademy.edu.au>
2023-10-17 20:34:33 +1100

    feat: async route with fetch working

---
+++ controllers/PokemonController.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+// create an instance of the Express router
+
+const router = express.Router();
+
+// this sends a json string. instead of just a html string
+router.get("/", (request, response) => {
+     response.json ({
+          message:"Hello world from a router!"
+     });
+});
+
+//async needs to be called before the await command
+router.get("/bananas", async (request, response) => {
+
+
+// the structure is await fetch, await result then the data response
+     let result = await fetch("https://pokeapi.co/api/v2/pokemon/25")
+     let data = await result.json();
+
+	response.json({
+		message: "bananas!",
+          pokemonName: data.name
+	});
+});
+
+// Create out of CRUD
+router.post("/", (request, response) => {
+     response.json({
+          message:"POST request received!"
+     })
+});
+
+
+
+
+module.exports = router;
+
+// can also be the following, but preferable syntax is above
+// module.exports = {
+//      PokemonRouter: router
+// }
--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const express = require('express');
 // make an instance of the server that we can customise and run
 const app = express();
 
+// this is an instance of the server as well, but not recommended to write this 
+// const express = require('express')(); 
+
+// const HOST = 'localhost';
+// const PORT = 3000
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3000;
+// Ports can only be a integer between 1001 - 65536
+
+
 // GET localhost:3000/
 // app.get(route path, callback_function)
 app.get("/", (request, response) => {
@@ -11,10 +22,15 @@ app.get("/", (request, response) => {
      response.send("Hello world, this server is bananas!")
 });
 
+const someImportedRouterYay = require('./controllers/PokemonController');
+app.use("/pokemon", someImportedRouterYay);
+
 //configure the server -- happens all above this line ^^^^^
 
 // ACtivate the server -- happens all below this line vvvvvv
 
-app.listen(3000,() => {
-     console.log("Server is running on port: " + 3000);
+
+// not good to hardcode to port 3000, so we can write PORT instead
+app.listen(PORT, HOST, () => {
+     console.log("Server is running on port: " + PORT);
 })
